710dc8df9c1c3c25cdd574738d03bebaaf3e6d02
Georgi Payov <110534798+GeorgiPayov@users.noreply.github.com>
2022-08-08 18:27:39 +0300

    Add files via upload

---
+++ C# OOP/Inheritance/1.Person/OOP Inheritance/Child.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Inheritance
+{
+    public class Child : Person
+    {
+        public Child (string name, int age)
+            : base(name, age)
+        {
+
+        }
+    }
+}
