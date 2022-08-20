b53e28d689aca5bbec7b3eab0a58f4c13e424894
Georgi Payov <110534798+GeorgiPayov@users.noreply.github.com>
2022-08-20 17:12:01 +0300

    Add files via upload

---
+++ C# OOP/Interfaces-and-Abstraction/4.Border-Control/Border-Control/Citizen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Border_Control
+{
+    public class Citizen
+    {
+        public Citizen(string name, int id)
+        {
+            this.Name = name;
+            this.Id = id;
+        }
+
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+    }
+}
---
+++ C# OOP/Interfaces-and-Abstraction/4.Border-Control/Border-Control/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Border_Control
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Citizen> citizen = new List<Citizen>();
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] input = command.Split();
+
+                if (input.Length > 2)
+                {
+                    string name = input[0] + input[1];
+                    int id = int.Parse(input[2]);
+                    Citizen person = new Citizen(name, id);
+                    citizen.Add(person);
+                }
+                else
+                {
+                    string name = input[0];
+                    int id = int.Parse(input[1]);
+                    Citizen person = new Citizen(name, id);
+                    citizen.Add(person);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            int fakeId = int.Parse(Console.ReadLine());
+
+            foreach (var person in citizen)
+            {
+                Console.WriteLine(person.Id);
+            }
+        }
+    }
+}
