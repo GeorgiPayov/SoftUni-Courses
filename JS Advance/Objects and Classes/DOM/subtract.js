2123d929e6f2de5575ea6e90be73e7c764ad773d
Georgi Payov <110534798+GeorgiPayov@users.noreply.github.com>
2022-09-28 22:29:04 +0300

    Delete subtract.js

--- JS Advance/Objects and Classes/DOM/subtract.js
+++
@@ -1,8 +0,0 @@
-function subtract() {
-    let firstNum = document.getElementById('firstNumber').value
-    let secondNum = document.getElementById('secondNumber').value
-    let result = document.getElementById('result')
-    result.textContent = Number(firstNum) - Number(secondNum);
-
-    console.log(result);
-}
