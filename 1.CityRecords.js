2dd5c422f2f6a951bdd26dd6e510a35181c269b8
Georgi Payov <110534798+GeorgiPayov@users.noreply.github.com>
2022-10-07 23:38:00 +0300

    Add files via upload

---
+++ 1.CityRecords.js
@@ -0,0 +1,15 @@
+function cityRecord (town, population, treasury) {
+
+    let city = {
+        name: town,
+        population: population,
+        treasury: treasury
+    }
+    
+    return city;
+}
+
+console.log(cityRecord('Tortuga', 7000, 15000))
+
+
+
